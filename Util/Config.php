903a219e39a9bab3ee7feb85ad141a450b6ee5c1
Jan Kramer <j.kramer@pronettix.nl>
2012-12-14 20:44:51 +0100

    Update Util/Config.php
    Spaces => tabs; Fix PHPDoc; make createNotFoundException protected

--- Util/Config.php
+++ Util/Config.php
@@ -44,51 +44,51 @@ class Config {
 		return $setting->getValue();
 	}
 
-    /**
-     * @param string $name Name of the setting.
-     * @param string|null $value Value of the setting.
-     * @throws \RuntimeException If setting is not defined.
-     */
-    public function set($name, $value) {
-        $setting = $this->getRepo()->findOneBy(array(
-            'name' => $name,
-        ));
+	/**
+	 * @param string $name Name of the setting.
+	 * @param string|null $value Value of the setting.
+	 * @throws \RuntimeException If setting is not defined.
+	 */
+	public function set($name, $value) {
+		$setting = $this->getRepo()->findOneBy(array(
+			'name' => $name,
+		));
 
-        if ($setting === null) {
-            throw $this->createNotFoundException($name);
-        }
+		if ($setting === null) {
+			throw $this->createNotFoundException($name);
+		}
 
-        $setting->setValue($value);
-        $this->em->flush($setting);
-    }
+		$setting->setValue($value);
+		$this->em->flush($setting);
+	}
 
-    /**
-     * @param array $newSettings List of settings (as name => value) to update
-     */
-    public function setMultiple(array $newSettings)
-    {
-        if (count(array($newSettings) == 0)) {
-            return;
-        }
+	/**
+	 * @param array $newSettings List of settings (as name => value) to update.
+	 */
+	public function setMultiple(array $newSettings)
+	{
+		if (empty($newSettings)) {
+			return;
+		}
 
-        $settings = $this->em->createQueryBuilder()
-            ->select('s')
-            ->from('CraueConfigBundle:Setting', 's', 's.name')
-            ->where('s.name IN (:names)')
-            ->getQuery()
-            ->execute(array('names' => array_keys($newSettings)))
-        ;
+		$settings = $this->em->createQueryBuilder()
+			->select('s')
+			->from('CraueConfigBundle:Setting', 's', 's.name')
+			->where('s.name IN (:names)')
+			->getQuery()
+			->execute(array('names' => array_keys($newSettings)))
+		;
 
-        foreach ($newSettings as $name => $value) {
-            if (!isset($settings[$name])) {
-                throw $this->createNotFoundException($name);
-            }
+		foreach ($newSettings as $name => $value) {
+			if (!isset($settings[$name])) {
+				throw $this->createNotFoundException($name);
+			}
 
-            $settings[$name]->setValue($value);
-        }
+			$settings[$name]->setValue($value);
+		}
 
-        $this->em->flush();
-    }
+		$this->em->flush();
+	}
 
 	/**
 	 * @return string[] with key => value
@@ -114,8 +114,12 @@ class Config {
 		return $this->repo;
 	}
 
-    private function createNotFoundException($name)
-    {
-        return new \RuntimeException(sprintf('Setting "%s" couldn\'t be found.', $name));
-    }
+	/**
+	 * @param string $name Name of the setting.
+	 * @return \RuntimeException 
+	 */
+	protected function createNotFoundException($name)
+	{
+		return new \RuntimeException(sprintf('Setting "%s" couldn\'t be found.', $name));
+	}
 }
