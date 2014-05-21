1937e8dacddaa2416c45534c0cd5cbed3fd5b0c4
Richard <rvanlaak@gmail.com>
2014-05-21 15:05:51 +0200

    * function brackets on same line - removed Dutch translation

--- Twig/Extension/ConfigTemplateExtension.php
+++ Twig/Extension/ConfigTemplateExtension.php
@@ -8,8 +8,7 @@ use Craue\ConfigBundle\Util\Config;
  * @copyright 2011-2014 Christian Raue
  * @license http://opensource.org/licenses/mit-license.php MIT License
  */
-class ConfigTemplateExtension extends \Twig_Extension 
-{
+class ConfigTemplateExtension extends \Twig_Extension {
 
 	/**
 	 * @var string[]
@@ -25,8 +24,7 @@ class ConfigTemplateExtension extends \Twig_Extension
 	 * Sets the order in which sections will be rendered.
 	 * @param string[] $sectionOrder
 	 */
-	public function setSectionOrder(array $sectionOrder = array()) 
-	{
+	public function setSectionOrder(array $sectionOrder = array()) {
 		$this->sectionOrder = $sectionOrder;
 	}
 
@@ -34,24 +32,21 @@ class ConfigTemplateExtension extends \Twig_Extension
 	 * Setter for Config class
 	 * @param Config $config
 	 */
-	public function setConfig($config) 
-	{
+	public function setConfig($config) {
 		$this->config = $config;
 	}
 
 	/**
 	 * {@inheritDoc}
 	 */
-	public function getName() 
-	{
+	public function getName() {
 		return 'craue_config_template';
 	}
 
 	/**
 	 * {@inheritDoc}
 	 */
-	public function getFilters() 
-	{
+	public function getFilters() {
 		return array(
 			'craue_sortSections' => new \Twig_Filter_Method($this, 'sortSections'),
 		);
@@ -60,8 +55,7 @@ class ConfigTemplateExtension extends \Twig_Extension
 	/**
 	 * {@inheritDoc}
 	 */
-	public function getFunctions()
-	{
+	public function getFunctions() {
 		return array(
 			'craue_setting' => new \Twig_Function_Function(array($this, 'getSetting')),
 		);
@@ -71,8 +65,7 @@ class ConfigTemplateExtension extends \Twig_Extension
 	 * @param string[] $sections
 	 * @return string[]
 	 */
-	public function sortSections(array $sections) 
-	{
+	public function sortSections(array $sections) {
 		$finalSectionOrder = array();
 
 		// add null section first (if it exists)
@@ -102,8 +95,7 @@ class ConfigTemplateExtension extends \Twig_Extension
 	 * @return Ambigous <string, NULL>
 	 * @throws \RuntimeException
 	 */
-	public function getSetting($name)
-	{
+	public function getSetting($name) {
 		return $this->config->get($name);
 	}
 
