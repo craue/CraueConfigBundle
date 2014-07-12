c89ba5529a9a55aa36d6e06accbf348ca88b4655
Alexandru Furculita <alex@rhetina.com>
2014-07-13 00:52:18 +0300

    Update Config.php

--- Util/Config.php
+++ Util/Config.php
@@ -27,7 +27,7 @@ class Config {
 		$this->em = $em;
 	}
 
- -	public function setEntityManager(EntityManager $em) {
+ 	public function setEntityManager(EntityManager $em) {
   		$this->em = $em;
   	}
 	/**
