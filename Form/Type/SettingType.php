59ce2c0bc7d318bfaddd25de123bcbb905afd68b
Christian Raue <christian.raue@gmail.com>
2017-04-03 09:13:50 +0200

    moved setting the label from the template to the form type

--- Form/Type/SettingType.php
+++ Form/Type/SettingType.php
@@ -4,6 +4,8 @@ namespace Craue\ConfigBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
@@ -24,6 +26,16 @@ class SettingType extends AbstractType {
 			'required' => false,
 			'translation_domain' => 'CraueConfigBundle',
 		));
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function finishView(FormView $view, FormInterface $form, array $options) {
+		/* @var $setting Setting */
+		$setting = $form->getData();
+
+		$view->children['value']->vars['label'] = $setting->getName();
 	}
 
 	/**
