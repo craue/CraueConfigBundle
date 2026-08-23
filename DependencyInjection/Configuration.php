15cbdeb5e8503ac087f3fc1c0dc100cd733d4b44
Christian Raue <christian.raue@gmail.com>
2026-08-23 21:49:13 +0200

    improved validation of bundle configuration

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -3,6 +3,7 @@
 namespace Craue\ConfigBundle\DependencyInjection;
 
 use Craue\ConfigBundle\Entity\Setting;
+use Craue\ConfigBundle\Entity\SettingInterface;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -29,8 +30,26 @@ class Configuration implements ConfigurationInterface {
 					->values($supportedDrivers)
 					->defaultValue($supportedDrivers[0])
 				->end()
+				// TODO change `scalarNode` to `stringNode` (and remove the custom string validation) as soon as Symfony >= 7.2 is required
 				->scalarNode('entity_name')
 					->defaultValue(Setting::class)
+					->cannotBeEmpty()
+					->validate()
+						->ifTrue(static fn(mixed $value) : bool => !is_string($value))
+						->thenInvalid('The value %s is not a string.')
+					->end()
+					->validate()
+						->ifTrue(static fn(string $class) : bool => !class_exists($class))
+						->thenInvalid('The class %s doesn\'t exist.')
+					->end()
+					->validate()
+						->ifTrue(static function(string $class) : bool {
+							/** @var class-string $class */
+							$reflection = new \ReflectionClass($class);
+							return $reflection->isAbstract() || !$reflection->implementsInterface(SettingInterface::class);
+						})
+						->thenInvalid('The class %s must be a non-abstract implementation of ' . SettingInterface::class . '.')
+					->end()
 				->end()
 			->end()
 		;
