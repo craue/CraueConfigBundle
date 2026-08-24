36ac83344532a95fb27d2d7cbc2b42c352f4c755
Christian Raue <christian.raue@gmail.com>
2026-08-24 17:11:22 +0200

    updated copyright year range

--- Tests/IntegrationTestBundle/Entity/CanBeDisabledSetting.php
+++ Tests/IntegrationTestBundle/Entity/CanBeDisabledSetting.php
@@ -6,7 +6,7 @@ use Craue\ConfigBundle\Entity\BaseSetting;
 
 /**
  * @author Christian Raue <christian.raue@gmail.com>
- * @copyright 2011-2023 Christian Raue
+ * @copyright 2011-2026 Christian Raue
  * @license http://opensource.org/licenses/mit-license.php MIT License
  */
 class CanBeDisabledSetting extends BaseSetting {
